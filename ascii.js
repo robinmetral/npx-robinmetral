a247e83425c8e5c900667e972ec66297eb52633c
Robin Métral <robin@metral.ch>
2019-04-27 14:40:42 +0200

    Add ASCII image
    From https://www.text-image.com/convert/pic2ascii.cgi

---
+++ ascii.js
@@ -0,0 +1,30 @@
+module.exports = `MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMNdyo////++shmMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMms:..---..---::smMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMNs:--------:++o+/::yNMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMhshdhhyyyydmmmdddyo:/mMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMNsddmmmmmmmmmmmmmmmd+-sNMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMNssddddmmmmmmmmmmmmdh/.-oNMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMNooddmmmmmmmmmmmmmmdh:..+NMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMNd/yossyyhdhso/+ooshd+``sNMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMNd+oo+:-:ymy//o++osyds.-omMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMNyhosossdmdysssyhdddy-+shMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMydddhdhdmdddddmmmddd/symMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMmdddhysyyosyhyhddddy:hmMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMNhy+o+::/::/oo+ohhhs:mMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMNhs/:-:+ooo+/:+syhy//NMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMmssy+ohhdhyyhhhyo/-omMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMd+oysooooyhhsso:-:hdNMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMN/-syo+oyhy/--..-shhhNNMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMh../o+oo/-````.oyhyddmNMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMNmd:.``````````.+syshhdddmNNMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMNNmdyy/-:.` ```.-/ooooyhddhhdhmmNNNMMMMMMMMMMM
+MMMMMMMMMNNNmddddhsyo.-///////++o++yhhhhhhdmmmmmmmmNNMMMMMMM
+MMMMMMNNmmmmdddddhs/:/.:///+++ooo+/oyhdhyhdmdmmmmmmmmmNNNMMM
+MMMMNmNmmmmdddddddsoy+-/+++++oooo///:+++osddddmmmmmmmmmdmmNM
+MMMNmmmmmdddhdddhhysy//+ssssssso+/osyyyyhhdhdddddmmmmmmdmNNN
+MMNmddmmdddhhdddhhhsyysoyyyyyysooosyyhhhddhddddddmdddhdmmmmm
+MMNmmhmdmddhddddhhhyyyoysyyyyys+oysyhddddhhddddddhhhhdmmmddm
+MMmmmddmddhddmddhhhhhhysssyyysooysyhdddddhdddddddyhydmdddddm
+MMmmmddddhddddddhhhhhhhhhysyoo+syhhdddddddddddddysyhdddddddd`;
